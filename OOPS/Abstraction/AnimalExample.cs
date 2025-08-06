0a8af3e75968101867a8e0f3bf007f67c02931f4
govind-qmanja <g3vind@qmanja.com>
2025-08-06 10:07:33 +0530

    abstraction

---
+++ OOPS/Abstraction/AnimalExample.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPS.Abstraction
+{
+    class AnimalExample
+    {
+        public abstract class Animal
+        {
+            public abstract void Speak();
+        }
+
+        public class Dog : Animal
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("Woof!");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("Meow!");
+            }
+        }
+    }
+}
